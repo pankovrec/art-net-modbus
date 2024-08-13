1ab10650b22ea5f4a4a5715d5ba89c8580742035
Evgeny <pankovrec@gmail.com>
2024-08-13 14:16:16 +0500

    art-net-work

--- main.py
+++ main.py
@@ -1,28 +1,29 @@
 
 import socket
-
+import time
 
 # create udp socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 # artnet device config
-server_address1 = ('192.168.0.99', 6454)
-server_address2 = ("192.168.0.100", 6454)
-
+server_address1 = ('127.0.0.1', 6454)
+server_address2 = ("127.0.0.1", 6455)
 
 # send packet from file
 with open('Test_Color.ani', 'rb') as f:
     while True:
-        packet = f.read(1000)
-      #
+        packet = f.read(530)
+
         if not packet:
             f.seek(0)
             continue
+
         universe = int(packet[14])
 
         if universe == 1:
 
             sock.sendto(packet, server_address1)
+            time.sleep(1/40)
 
         else:
             sock.sendto(packet, server_address2)
